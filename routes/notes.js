c8166c7bd2940c319d0e8bb0d5955507cf8cefb5
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-05-02 15:40:57 -0400

    added helpers functions

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,32 @@
-const { Router } = require('express');
+const notes = require('express').Router();
 
-const router = Router();
+notes.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
 
-module.exports = router;
+notes.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting feedback');
+    }
+  });
+
+module.exports = notes;
