51fb1684b048a10c1a88a75372f8e367a045a604
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-05-02 18:58:53 -0400

    added a delete button

--- routes/notes.js
+++ routes/notes.js
@@ -1,35 +1,51 @@
 const notes = require('express').Router();
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 notes.get('/', (req, res) =>
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 notes.post('/', (req, res) => {
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
-  
+
     // If all the required properties are present
     if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        note_id: uuid()
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      res.json(response);
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
     } else {
-      res.json('Error in posting feedback');
+        res.json('Error in posting feedback');
     }
-  });
+});
+
+notes.delete('/:id', async (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        // Make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+});
 
 module.exports = notes;
