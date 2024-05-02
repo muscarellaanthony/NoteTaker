ad08466dc3e93e811639191c3922b09bdbeb16f0
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-05-01 22:59:11 -0400

    added path to change pages on site

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const notesRoute = require('./routes/notes');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () => console.log(`running express server on port ${PORT}`));
